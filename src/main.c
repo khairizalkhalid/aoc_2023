fa7e6c65b3a006cb5f9411eea80b4481d0a19890
khairizalkhalid <khairizalkhalid@outlook.com>
2025-08-03 22:57:01 +0800

    add base program to run each of the challanges by day.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+
+void day1_part1() { printf("Running Day 1 Part 1\n"); }
 
 int main() {
 	printf("Advent of Code 2023 in C\n");
-	return 0;
+    char input[16];
+
+    printf("Please enter program to run ie: d1p1, d1p2, d2p1, d2p2, etc.\n");
+    printf("> ");
+    if (scanf("%15s", input) != 1) {
+        printf("Input error.\n");
+        return 1;
+    }
+
+    if (strcmp(input, "d1p1") == 0) {
+        day1_part1();
+    } else {
+        printf("Unknown program: %s\n", input);
+    }
+
+    return 0;
 }
